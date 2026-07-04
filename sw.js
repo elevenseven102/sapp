e2c4aa92c6c0f12b416e2c493bee7bf4c960f84d
elevenseven102 <badlycoder@gmail.com>
2026-07-04 16:54:19 +0300

    33

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
-const CACHE = 'doc-scanner-v3';
-const URLS = [
+const CACHE_NAME = 'scanbot-pwa-v1';
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/app.js',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
-  'https://docs.opencv.org/4.7.0/opencv.js',
-  'https://cdn.jsdelivr.net/gh/ColonelParrot/jscanify@master/src/jscanify.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+  'https://cdn.jsdelivr.net/npm/scanbot-web-sdk@7.0.0/bundle/ScanbotSDK.ui2.min.js',
+  // Кеширование всех ресурсов движка (можно перечислить или использовать динамическое добавление)
+  // Для простоты мы используем стратегию: при установке кешируем только основные файлы,
+  // а остальные (engine) будут добавляться в кеш по мере использования.
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(URLS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      // Возвращаем из кеша, если есть, иначе идём в сеть и сохраняем в кеш
+      return cachedResponse || fetch(event.request).then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
 });
